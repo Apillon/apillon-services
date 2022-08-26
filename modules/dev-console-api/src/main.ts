f0c9425f3cfa73fe5842d4e9d036b06357b5d809
Tine Mlakar <tine.mlakar@kalmia.si>
2022-08-26 16:00:44 +0200

    turborepo, eslint & ts configs, workspaces

---
+++ modules/dev-console-api/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(6001);
+  console.log('Listening on 6001');
+}
+bootstrap();
