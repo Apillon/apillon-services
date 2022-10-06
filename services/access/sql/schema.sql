f963fe9583e574ae2e65b40d3a0f722f542b93a0
Luka Golinar <luka.golinar@kalmia.si>
2022-10-06 15:17:29 +0200

    feat: authToken table.

--- services/access/sql/schema.sql
+++ services/access/sql/schema.sql
@@ -153,6 +153,20 @@ CREATE TABLE IF NOT EXISTS `apiKey_role` (
     ON DELETE CASCADE
     ON UPDATE NO ACTION);
 
+-- -----------------------------------------------------
+-- Table `authToken`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `authToken` (
+  `authToken_id` INT NOT NULL AUTO_INCREMENT,
+  `token` TEXT NOT NULL,
+  `user_uuid` VARCHAR(36) NOT NULL,
+  `expiresAt` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` INT NULL,
+  `createTime` DATETIME NULL,
+  `createUser` INT NULL,
+  `updateTime` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updateUser` INT NULL,
+  PRIMARY KEY (`authToken_id`));
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
