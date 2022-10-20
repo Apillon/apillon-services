a82fae3291835d056433f91643a138db9b4b7f2c
Tine Mlakar <tine.mlakar@kalmia.si>
2022-10-20 22:37:31 +0200

    build config

---
+++ bin/deploy/clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Cleaning environmentr..."
+TIMESTAMP="$(date +%s)"
+echo $TIMESTAMP
+
+
+# clean the environemnt
+rm node_modules -r
+cd packages/at-lib/
+rm node_modules -r
+cd ../../modules/dev-console-api/
+rm node_modules -r
+cd ../../services/access/
+rm node_modules -r
+cd ../../services/monitoring/
+rm node_modules -r
+cd ../..
+
