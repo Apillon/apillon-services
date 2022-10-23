6739a2c8ccb8bcbd4d1fd3f7f2936c3af904f3fe
vinkos <vinko.smid@kalmia.si>
2022-10-23 14:05:11 +0200

    at-storage Directory CUD functions. Dev-console-api directory module.

---
+++ services/storage/src/modules/directory/models/directory.model.ts
@@ -0,0 +1,163 @@
+import { integerParser, stringParser } from '@rawmodel/parsers';
+import { presenceValidator } from '@rawmodel/validators';
+import {
+  AdvancedSQLModel,
+  Context,
+  PopulateFrom,
+  prop,
+  SerializeFor,
+} from 'at-lib';
+import { v4 as uuidV4 } from 'uuid';
+import { DbTables, StorageErrorCode } from '../../../config/types';
+
+export class Directory extends AdvancedSQLModel {
+  public readonly tableName = DbTables.DIRECTORY;
+
+  public constructor(data: any, context: Context) {
+    super(data, context);
+  }
+
+  @prop({
+    parser: { resolver: stringParser() },
+    populatable: [
+      PopulateFrom.DB,
+      PopulateFrom.SERVICE,
+      PopulateFrom.ADMIN,
+      PopulateFrom.PROFILE,
+    ],
+    serializable: [
+      SerializeFor.INSERT_DB,
+      SerializeFor.ADMIN,
+      SerializeFor.SERVICE,
+      SerializeFor.PROFILE,
+    ],
+    validators: [],
+    defaultValue: uuidV4(),
+    fakeValue: uuidV4(),
+  })
+  public directory_uuid: string;
+
+  @prop({
+    parser: { resolver: integerParser() },
+    populatable: [
+      PopulateFrom.DB,
+      PopulateFrom.SERVICE,
+      PopulateFrom.ADMIN,
+      PopulateFrom.PROFILE,
+    ],
+    serializable: [
+      SerializeFor.INSERT_DB,
+      SerializeFor.UPDATE_DB,
+      SerializeFor.ADMIN,
+      SerializeFor.SERVICE,
+      SerializeFor.PROFILE,
+    ],
+    validators: [],
+  })
+  public parentDirectory_id: string;
+
+  @prop({
+    parser: { resolver: integerParser() },
+    populatable: [
+      PopulateFrom.DB,
+      PopulateFrom.SERVICE,
+      PopulateFrom.ADMIN,
+      PopulateFrom.PROFILE,
+    ],
+    serializable: [
+      SerializeFor.INSERT_DB,
+      SerializeFor.ADMIN,
+      SerializeFor.SERVICE,
+      SerializeFor.PROFILE,
+    ],
+    validators: [
+      {
+        resolver: presenceValidator(),
+        code: StorageErrorCode.DIRECTORY_BUCKET_ID_NOT_PRESENT,
+      },
+    ],
+  })
+  public bucket_id: string;
+
+  @prop({
+    parser: { resolver: stringParser() },
+    populatable: [
+      PopulateFrom.DB,
+      PopulateFrom.SERVICE,
+      PopulateFrom.ADMIN,
+      PopulateFrom.PROFILE,
+    ],
+    serializable: [
+      SerializeFor.INSERT_DB,
+      SerializeFor.ADMIN,
+      SerializeFor.SERVICE,
+      SerializeFor.PROFILE,
+    ],
+    validators: [],
+  })
+  public CID: string;
+
+  @prop({
+    parser: { resolver: stringParser() },
+    populatable: [
+      PopulateFrom.DB,
+      PopulateFrom.SERVICE,
+      PopulateFrom.ADMIN,
+      PopulateFrom.PROFILE,
+    ],
+    serializable: [
+      SerializeFor.ADMIN,
+      SerializeFor.INSERT_DB,
+      SerializeFor.UPDATE_DB,
+      SerializeFor.SERVICE,
+      SerializeFor.PROFILE,
+      SerializeFor.SELECT_DB,
+    ],
+    validators: [
+      {
+        resolver: presenceValidator(),
+        code: StorageErrorCode.DIRECTORY_NAME_NOT_PRESENT,
+      },
+    ],
+  })
+  public name: string;
+
+  @prop({
+    parser: { resolver: stringParser() },
+    populatable: [
+      PopulateFrom.DB,
+      PopulateFrom.SERVICE,
+      PopulateFrom.ADMIN,
+      PopulateFrom.PROFILE,
+    ],
+    serializable: [
+      SerializeFor.ADMIN,
+      SerializeFor.INSERT_DB,
+      SerializeFor.UPDATE_DB,
+      SerializeFor.SERVICE,
+      SerializeFor.PROFILE,
+      SerializeFor.SELECT_DB,
+    ],
+    validators: [],
+  })
+  public description: string;
+
+  /*public async getList(context: ServiceContext, query: BucketQueryFilter) {
+    const params = {
+      project_uuid: query.project_uuid,
+      search: query.search,
+    };
+
+    const sqlQuery = {
+      qSelect: `
+        SELECT ${this.generateSelectFields('b', '')}
+        `,
+      qFrom: `
+        FROM \`${DbTables.BUCKET}\` b
+        WHERE (@search IS null OR b.name LIKE CONCAT('%', @search, '%'))
+      `,
+    };
+
+    return selectAndCountQuery(context.mysql, sqlQuery, params, 'b.id');
+  }*/
+}
