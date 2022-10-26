166428651ed6db8aa4cd7f040304f1a1e2326d2f
Tine Mlakar <tine.mlakar@kalmia.si>
2022-10-26 13:17:28 +0200

    lambda fix

--- modules/dev-console-api/src/lambda.ts
+++ modules/dev-console-api/src/lambda.ts
@@ -6,12 +6,16 @@ import * as express from 'express';
 
 import { Server } from 'http';
 import { AppModule } from './app.module';
+import { ExceptionsFilter } from './filters/exception.filter';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 export async function bootstrap() {
   const expressApp = express();
   const adapter = new ExpressAdapter(expressApp);
   const app = await NestFactory.create(AppModule, adapter);
   app.enableCors({ origin: '*' });
+  app.useGlobalFilters(new ExceptionsFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.init();
   return createServer(expressApp);
 }
