5418cda5fc4dcdfdfa0bd95a150670b18badf5bb
Tine Mlakar <tine.mlakar@kalmia.si>
2022-10-26 11:43:14 +0200

    exception filter fix

--- modules/dev-console-api/src/filters/exception.filter.ts
+++ modules/dev-console-api/src/filters/exception.filter.ts
@@ -4,7 +4,7 @@ import {
   ExceptionFilter,
   HttpStatus,
 } from '@nestjs/common';
-import { SystemErrorCode, CodeException, ValidationException } from 'at-lib';
+import { SystemErrorCode } from 'at-lib';
 import { Request, Response } from 'express';
 
 @Catch()
@@ -14,31 +14,51 @@ export class ExceptionsFilter implements ExceptionFilter {
     const res = exceptionCtx.getResponse<Response>();
     const request = exceptionCtx.getRequest<Request>();
 
-    if (error instanceof CodeException) {
+    if (error?.getStatus()) {
       res.status(error.getStatus()).json({
         status: error.getStatus(),
-        code: error.getResponse()['code'],
-        message: error.message,
-        origin: error.origin,
-        path: request.url,
+        code: error?.getResponse()?.code,
+        message: error?.message,
+        origin: error?.origin,
+        path: request?.url,
         timestamp: new Date().toISOString(),
-      });
-    } else if (error instanceof ValidationException) {
-      res.status(error.getStatus()).json({
-        status: error.getStatus(),
-        model: error.modelName,
-        errors: error.errors,
-        path: request.url,
-        timestamp: new Date().toISOString(),
+        model: error?.modelName || undefined,
       });
     } else {
       res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
         status: HttpStatus.INTERNAL_SERVER_ERROR,
-        code: error.code || SystemErrorCode.UNHANDLED_SYSTEM_ERROR,
-        message: error.message,
-        path: request.url,
+        code: error?.code || SystemErrorCode.UNHANDLED_SYSTEM_ERROR,
+        message: error?.message,
+        path: request?.url,
         timestamp: new Date().toISOString(),
       });
     }
+
+    // if (error instanceof CodeException) {
+    //   res.status(error.getStatus()).json({
+    //     status: error.getStatus(),
+    //     code: error.getResponse()['code'],
+    //     message: error.message,
+    //     origin: error.origin,
+    //     path: request.url,
+    //     timestamp: new Date().toISOString(),
+    //   });
+    // } else if (error instanceof ValidationException) {
+    //   res.status(error.getStatus()).json({
+    //     status: error.getStatus(),
+    //     model: error.modelName,
+    //     errors: error.errors,
+    //     path: request.url,
+    //     timestamp: new Date().toISOString(),
+    //   });
+    // } else {
+    //   res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+    //     status: HttpStatus.INTERNAL_SERVER_ERROR,
+    //     code: error.code || SystemErrorCode.UNHANDLED_SYSTEM_ERROR,
+    //     message: error.message,
+    //     path: request.url,
+    //     timestamp: new Date().toISOString(),
+    //   });
+    // }
   }
 }
