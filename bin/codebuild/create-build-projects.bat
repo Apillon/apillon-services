65026277b064d9834698e9bd1ee0ede1d191ecb0
Tine Mlakar <tine.mlakar@kalmia.si>
2022-11-08 17:14:41 +0100

    codebuild configurations

--- bin/codebuild/create-build-projects.bat
+++ bin/codebuild/create-build-projects.bat
@@ -1,2 +1,15 @@
-aws codebuild create-project --cli-input-json file://dev-console-config-dev.json
-aws codebuild create-webhook --cli-input-json file://dev-console-webhook-dev.json
+@REM aws codebuild create-project --cli-input-json file://dev-console-config-dev.json
+@REM aws codebuild create-webhook --cli-input-json file://dev-console-webhook-dev.json
+
+@REM aws codebuild create-project --cli-input-json file://access-config-dev.json
+aws codebuild create-webhook --cli-input-json file://access-webhook-dev.json
+
+aws codebuild create-project --cli-input-json file://monitoring-config-dev.json
+aws codebuild create-webhook --cli-input-json file://monitoring-webhook-dev.json
+
+aws codebuild create-project --cli-input-json file://storage-config-dev.json
+aws codebuild create-webhook --cli-input-json file://storage-webhook-dev.json
+
+aws codebuild create-project --cli-input-json file://mailing-config-dev.json
+aws codebuild create-webhook --cli-input-json file://mailing-webhook-dev.json
+
