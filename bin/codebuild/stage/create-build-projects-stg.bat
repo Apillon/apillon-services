e1aae466e76c070bdd92303fe0f27b63b939d2de
Tine Mlakar <tine.mlakar@kalmia.si>
2022-11-14 13:58:04 +0100

    staging codebuild configurations

---
+++ bin/codebuild/stage/create-build-projects-stg.bat
@@ -0,0 +1,15 @@
+aws codebuild create-project --cli-input-json file://dev-console-config-stg.json --profile authtrail
+aws codebuild create-webhook --cli-input-json file://dev-console-webhook-stg.json --profile authtrail
+
+aws codebuild create-project --cli-input-json file://access-config-stg.json --profile authtrail
+aws codebuild create-webhook --cli-input-json file://access-webhook-stg.json --profile authtrail
+
+aws codebuild create-project --cli-input-json file://monitoring-config-stg.json --profile authtrail
+aws codebuild create-webhook --cli-input-json file://monitoring-webhook-stg.json --profile authtrail
+
+aws codebuild create-project --cli-input-json file://storage-config-stg.json --profile authtrail
+aws codebuild create-webhook --cli-input-json file://storage-webhook-stg.json --profile authtrail
+
+aws codebuild create-project --cli-input-json file://mailing-config-stg.json --profile authtrail
+aws codebuild create-webhook --cli-input-json file://mailing-webhook-stg.json --profile authtrail
+
