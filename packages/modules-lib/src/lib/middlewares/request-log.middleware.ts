002f2786fece957e19493139151a64137188654c
Tine Mlakar <tine.mlakar@kalmia.si>
2022-11-23 12:05:33 +0100

    header test

--- packages/modules-lib/src/lib/middlewares/request-log.middleware.ts
+++ packages/modules-lib/src/lib/middlewares/request-log.middleware.ts
@@ -56,6 +56,9 @@ export function createRequestLogMiddleware(
               null,
           });
           await new Lmas().writeRequestLog(request);
+          console.log(`HEADERS: ${JSON.stringify(req.headers)}`);
+          console.log(`REQ: ${JSON.stringify(req)}`);
+          console.log(`CONTEXT: ${JSON.stringify(context)}`);
         } catch (error) {
           console.log('error:', error);
         }
