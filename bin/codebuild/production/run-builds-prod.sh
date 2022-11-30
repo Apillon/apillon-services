d37153d515cd650d277d5bd65c05638449895a2d
Tine Mlakar <tine.mlakar@kalmia.si>
2022-11-30 13:13:08 +0100

    minor fixes

--- bin/codebuild/production/run-builds-prod.sh
+++ bin/codebuild/production/run-builds-prod.sh
@@ -3,3 +3,4 @@ aws codebuild start-build --project-name apillon-monitoring-production
 aws codebuild start-build --project-name apillon-mailing-production
 aws codebuild start-build --project-name apillon-dev-console-api-production
 aws codebuild start-build --project-name apillon-storage-production
+
