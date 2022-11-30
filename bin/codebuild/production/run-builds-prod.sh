17d077735de1117c19cd8522ba2233bed3bec180
Tine Mlakar <tine.mlakar@kalmia.si>
2022-11-30 12:49:35 +0100

    deployment configurations and docs

---
+++ bin/codebuild/production/run-builds-prod.sh
@@ -0,0 +1,5 @@
+aws codebuild start-build --project-name apillon-access-production
+aws codebuild start-build --project-name apillon-monitoring-production
+aws codebuild start-build --project-name apillon-mailing-production
+aws codebuild start-build --project-name apillon-dev-console-api-production
+aws codebuild start-build --project-name apillon-storage-production
