a8a2676dfa272cf087af8f0d585be03d7a7491a8
Luka Golinar <luka.golinar@kalmia.si>
2023-01-04 17:09:41 +0100

    feat: Test identity generate endpoint, Test GET identity generation process STATE, mock file

--- modules/authentication-api/src/modules/identity/tests/authentication.e2e.test.ts
+++ modules/authentication-api/src/modules/identity/tests/authentication.e2e.test.ts
@@ -1,43 +1,25 @@
 import * as request from 'supertest';
-import {
-  createTestUser,
-  releaseStage,
-  Stage,
-  TestUser,
-} from '@apillon/tests-lib';
+import { releaseStage, Stage } from '@apillon/tests-lib';
 import { setupTest } from '../../../../test/helpers/setup';
 import { IdentityState, JwtTokenType } from '../../../config/types';
 import { generateJwtToken, SerializeFor } from '@apillon/lib';
 import { Identity } from '../models/identity.model';
 import { AuthenticationApiContext } from '../../../context';
-
-/*
-"errors": Array [
-  Object {
-    "message": "USER_EMAIL_NOT_PRESENT",
-    "property": "email",
-    "statusCode": 422070002,
-  },
-],
-*/
+import * as mock from './mock';
 
 describe('Authentication tests', () => {
   let stage: Stage;
   let context: AuthenticationApiContext;
   jest.setTimeout(6000 * 10); // Set timeout to 1 minute
 
+  // CONTROL PARAMETERS
   const controlMailInvalid = 'INVALIDMAIL';
   const controlMailInvalidDomain = 'test@123@#.www';
-
-  let testUser: TestUser;
-  let testUser2: TestUser;
 
   beforeAll(async () => {
     console.log('Setup stage ...');
     stage = await setupTest();
     context = stage.authApiContext;
-    testUser = await createTestUser(stage.devConsoleContext, stage.amsContext);
-    testUser2 = await createTestUser(stage.devConsoleContext, stage.amsContext);
   });
 
   afterAll(async () => {
@@ -45,169 +27,224 @@ describe('Authentication tests', () => {
     jest.setTimeout(5000); // 5 seconds -> revert
   });
 
-  // Attestation:
-  // - startUserAttestationProcess
-  //   -> email combinations (valid, invalid, empty, existing)
-  //   -> expects a mail sent to address (Not sure how we can check that)
-  // - getUserAttestationState
-  //   -> email combinations
-  // - generateIdentity
-  //   -> input parameters combinations (did_create_op, email, didUri), token data combinations
-  // - getUserCredential
-  //   -> email combinations (valid, empty, existing, non-existing)
-  // Verification:
-  // - verifyIdentity
-
   describe('ATTESTATION', () => {
-    test('Test init of identity generation process', async () => {
-      const resp = await request(stage.http).post('/identity/start').send({
-        email: '',
-      });
-      // SUBCASE 1: EMPTY BODY
-      expect(resp.status).toBe(422);
-      const data1 = JSON.parse(resp.text);
-      expect(data1.errors.length).toEqual(1);
-      const error1 = data1.errors[0];
+    describe('Init identity generation process', () => {
+      test('Empty email', async () => {
+        const resp = await request(stage.http).post('/identity/start').send({
+          email: '',
+        });
+        // SUBCASE 1: EMPTY BODY
+        expect(resp.status).toBe(422);
+        const data1 = JSON.parse(resp.text);
+        expect(data1.errors.length).toEqual(1);
+        const error1 = data1.errors[0];
 
-      expect(error1.message).toEqual('USER_EMAIL_NOT_PRESENT');
-      expect(error1.property).toEqual('email');
-      expect(error1.statusCode).toEqual(422070002);
-
-      // SUBCASE 2: INVALID EMAIL
-      const resp2 = await request(stage.http).post('/identity/start').send({
-        email: controlMailInvalid,
-      });
-      expect(resp2.status).toBe(422);
-
-      const data2 = JSON.parse(resp2.text);
-      expect(data2.errors.length).toEqual(1);
-      const error2 = data2.errors[0];
-
-      expect(error2.message).toEqual('USER_EMAIL_NOT_VALID');
-      expect(error2.property).toEqual('email');
-      expect(error2.statusCode).toEqual(422070003);
-
-      // SUBCASE 3: INVALID MAIL DOMAIN
-      const resp3 = await request(stage.http).post('/identity/start').send({
-        email: controlMailInvalidDomain,
-      });
-      expect(resp3.status).toBe(422);
-
-      const data3 = JSON.parse(resp3.text);
-      expect(data3.errors.length).toEqual(1);
-      const error3 = data3.errors[0];
-
-      expect(error3.message).toEqual('USER_EMAIL_NOT_VALID');
-      expect(error3.property).toEqual('email');
-      expect(error3.statusCode).toEqual(422070003);
-
-      // SUBCASE 4: VALID EMAIL
-      const resp4 = await request(stage.http).post('/identity/start').send({
-        email: 'hello@mailinator.com',
-      });
-      expect(resp4.status).toBe(201);
-
-      const data4 = JSON.parse(resp4.text);
-      expect(data4.errors).toEqual(undefined);
-
-      // SUBCASE 5: 1. VALID EMAIL, 2. ATTESTATION EXISTS
-      const testEmail = 'test3@mailinator.com';
-      const token = generateJwtToken(JwtTokenType.IDENTITY_EMAIL_VERIFICATION, {
-        testEmail,
+        expect(error1.message).toEqual('USER_EMAIL_NOT_PRESENT');
+        expect(error1.property).toEqual('email');
+        expect(error1.statusCode).toEqual(422070002);
       });
 
-      // So we make sure the object is commited to the database right away
-      const identity = new Identity({}, context);
-      identity.populate({
-        context: context,
-        email: testEmail,
-        state: IdentityState.ATTESTED,
-        token: token,
-        credential: { props: 'I am a credential lalala' },
-        status: 5,
+      test('Invalid email', async () => {
+        // SUBCASE 2: INVALID EMAIL
+        const resp2 = await request(stage.http).post('/identity/start').send({
+          email: controlMailInvalid,
+        });
+        expect(resp2.status).toBe(422);
+
+        const data2 = JSON.parse(resp2.text);
+        expect(data2.errors.length).toEqual(1);
+        const error2 = data2.errors[0];
+
+        expect(error2.message).toEqual('USER_EMAIL_NOT_VALID');
+        expect(error2.property).toEqual('email');
+        expect(error2.statusCode).toEqual(422070003);
       });
-      await identity.insert(SerializeFor.INSERT_DB);
 
-      const identityDb = await new Identity({}, context).populateByUserEmail(
-        context,
-        testEmail,
-      );
+      test('Invalid email domain', async () => {
+        // SUBCASE 3: INVALID MAIL DOMAIN
+        const resp3 = await request(stage.http).post('/identity/start').send({
+          email: controlMailInvalidDomain,
+        });
+        expect(resp3.status).toBe(422);
 
-      expect(identityDb).not.toBeUndefined();
-      expect(identityDb.email).toEqual(testEmail);
-      expect(identityDb.state).toEqual(IdentityState.ATTESTED);
+        const data3 = JSON.parse(resp3.text);
+        expect(data3.errors.length).toEqual(1);
+        const error3 = data3.errors[0];
 
-      const resp5 = await request(stage.http).post('/identity/start').send({
-        email: testEmail,
+        expect(error3.message).toEqual('USER_EMAIL_NOT_VALID');
+        expect(error3.property).toEqual('email');
+        expect(error3.statusCode).toEqual(422070003);
       });
-      expect(resp5.status).toBe(201);
 
-      const data5 = JSON.parse(resp5.text);
+      test('Valid email', async () => {
+        // SUBCASE 4: VALID EMAIL
+        const resp4 = await request(stage.http).post('/identity/start').send({
+          email: 'hello@mailinator.com',
+        });
+        expect(resp4.status).toBe(201);
 
-      // We send no errors, as this is expected behaviour
-      expect(data5.errors).toEqual(undefined);
-      // Success, however, must be false
-      expect(data5.data.success).toBeFalsy();
-      expect(data5.data.message).toEqual('Email already attested');
+        const data4 = JSON.parse(resp4.text);
+        expect(data4.errors).toEqual(undefined);
+      });
+
+      test('Valid email, but attestation exists', async () => {
+        // SUBCASE 5: 1. VALID EMAIL, 2. ATTESTATION EXISTS
+        const testEmail = 'test3@mailinator.com';
+        const token = generateJwtToken(
+          JwtTokenType.IDENTITY_EMAIL_VERIFICATION,
+          {
+            testEmail,
+          },
+        );
+
+        // So we make sure the object is commited to the database right away
+        const identity = new Identity({}, context);
+        identity.populate({
+          context: context,
+          email: testEmail,
+          state: IdentityState.ATTESTED,
+          token: token,
+          credential: { props: 'I am a credential lalala' },
+          status: 5,
+        });
+        await identity.insert(SerializeFor.INSERT_DB);
+
+        const identityDb = await new Identity({}, context).populateByUserEmail(
+          context,
+          testEmail,
+        );
+
+        expect(identityDb).not.toBeUndefined();
+        expect(identityDb.email).toEqual(testEmail);
+        expect(identityDb.state).toEqual(IdentityState.ATTESTED);
+
+        const resp5 = await request(stage.http).post('/identity/start').send({
+          email: testEmail,
+        });
+        expect(resp5.status).toBe(201);
+
+        const data5 = JSON.parse(resp5.text);
+
+        // We send no errors, as this is expected behaviour
+        expect(data5.errors).toEqual(undefined);
+        // Success, however, must be false
+        expect(data5.data.success).toBeFalsy();
+        expect(data5.data.message).toEqual('Email already attested');
+      });
     });
 
-    test('Test identity get state endpoint', async () => {
-      // const testUserMail = testUser.user.email;
-      // // VALID EMAIL
-      // const resp = await request(stage.http).post('/identity/start').send({
-      //   email: testUserMail,
-      // });
-      // expect(resp.status).toBe(201);
-      // const data = JSON.parse(resp.text);
-      // expect(data.errors).toEqual(undefined);
-      // const error = data.errors[0];
-      // expect(error.message).toEqual('USER_EMAIL_NOT_VALID');
-      // expect(error.property).toEqual('email');
-      // expect(error.statusCode).toEqual(422070003);
-      // const response = await request(stage.http).get('/identity/state').send({
-      //   email: '',
-      // });
-      // // Empty body
-      // expect(resp.status).toBe(422);
-      // const data1 = JSON.parse(resp.text);
-      // expect(data1.errors.length).toEqual(1);
-      // const error1 = data1.errors[0];
-      // expect(error1.message).toEqual('USER_EMAIL_NOT_PRESENT');
-      // expect(error1.property).toEqual('email');
-      // expect(error1.statusCode).toEqual(422070002);
-      // // INVALID EMAIL
-      // const resp2 = await request(stage.http).get('/identity/state').send({
-      //   email: controlMailInvalid,
-      // });
-      // expect(resp2.status).toBe(422);
-      // const data2 = JSON.parse(resp2.text);
-      // expect(data2.errors.length).toEqual(1);
-      // const error2 = data2.errors[0];
-      // expect(error2.message).toEqual('USER_EMAIL_NOT_VALID');
-      // expect(error2.property).toEqual('email');
-      // expect(error2.statusCode).toEqual(422070003);
-      // // INVALID MAIL DOMAIN
-      // const resp3 = await request(stage.http).get('/identity/state').send({
-      //   email: controlMailInvalidDomain,
-      // });
-      // expect(resp3.status).toBe(422);
-      // const data3 = JSON.parse(resp3.text);
-      // expect(data3.errors.length).toEqual(1);
-      // const error3 = data3.errors[0];
-      // expect(error3.message).toEqual('USER_EMAIL_NOT_VALID');
-      // expect(error3.property).toEqual('email');
-      // expect(error3.statusCode).toEqual(422070003);
-      // // VALID EMAIL
-      // const resp4 = await request(stage.http).get('/identity/state').send({
-      //   email: 'test@mailinator.com',
-      // });
-      // expect(resp4.status).toBe(201);
-      // const data4 = JSON.parse(resp4.text);
-      // expect(data4.errors).toEqual(undefined);
-      // const error4 = data3.errors[0];
-      // expect(error4.message).toEqual('USER_EMAIL_NOT_VALID');
-      // expect(error4.property).toEqual('email');
-      // expect(error4.statusCode).toEqual(422070003);
+    describe('Test GET identity generation process STATE', () => {
+      const testEmail = 'test_state@mailinator.com';
+
+      test('Test INVALID email', async () => {
+        const resp = await request(stage.http)
+          .get(`/identity/state?email=${controlMailInvalid}`)
+          .send();
+        expect(resp.status).toBe(422);
+      });
+
+      test('Test INVALID email domain', async () => {
+        const resp = await request(stage.http)
+          .get(`/identity/state?email=${controlMailInvalidDomain}`)
+          .send();
+        expect(resp.status).toBe(422);
+      });
+
+      test('Process not started for email - NO ENTRY IN DB', async () => {
+        // STATE --> No entry for this email
+        const resp = await request(stage.http)
+          .get(`/identity/state?email=${testEmail}`)
+          .send();
+        expect(resp.status).toBe(400);
+      });
+
+      test('Process for email - IN-PROGRESS', async () => {
+        // STATE --> in-progress
+        const resp1 = await request(stage.http).post('/identity/start').send({
+          email: testEmail,
+        });
+        expect(resp1.status).toBe(201);
+
+        const data4 = JSON.parse(resp1.text);
+        expect(data4.errors).toEqual(undefined);
+
+        const resp2 = await request(stage.http)
+          .get(`/identity/state?email=${testEmail}`)
+          .send();
+        expect(resp2.status).toBe(200);
+
+        const data = JSON.parse(resp2.text).data;
+        expect(data.state).toEqual('in-progress');
+
+        const identity = await new Identity({}, context).populateByUserEmail(
+          context,
+          testEmail,
+        );
+
+        identity.populate({
+          state: 'attested',
+        });
+        await identity.update();
+
+        const identityDb = await new Identity({}, context).populateByUserEmail(
+          context,
+          testEmail,
+        );
+
+        expect(identityDb).not.toBeUndefined();
+        expect(identityDb.email).toEqual(testEmail);
+        expect(identityDb.state).toEqual(IdentityState.ATTESTED);
+      });
+
+      test('Process is finished for email - ATTESTED', async () => {
+        // STATE --> attested
+        const resp3 = await request(stage.http)
+          .get(`/identity/state?email=${testEmail}`)
+          .send();
+        expect(resp3.status).toBe(200);
+
+        const data3 = JSON.parse(resp3.text).data;
+        expect(data3.state).toEqual('attested');
+      });
+    });
+
+    describe('Test identity generate endpoint', () => {
+      // input parameters combinations (did_create_op, email, didUri), token data combinations
+      test('Email combinations', async () => {
+        const testEmail = 'test@mailinator.com';
+        const token = generateJwtToken(
+          JwtTokenType.IDENTITY_EMAIL_VERIFICATION,
+          {
+            testEmail,
+          },
+        );
+
+        // So we make sure the object is commited to the database right away
+        const identity = new Identity({}, context);
+        identity.populate({
+          context: context,
+          email: testEmail,
+          state: IdentityState.IN_PROGRESS,
+          token: token,
+          credential: { props: 'I am a credential lalala' },
+          status: 5,
+        });
+        await identity.insert(SerializeFor.INSERT_DB);
+
+        const identityDb = await new Identity({}, context).populateByUserEmail(
+          context,
+          testEmail,
+        );
+
+        expect(identityDb).not.toBeUndefined();
+        expect(identityDb.email).toEqual(testEmail);
+        expect(identityDb.state).toEqual(IdentityState.IN_PROGRESS);
+
+        const resp = await request(stage.http)
+          .get(`/identity/state?email=${testEmail}`)
+          .send();
+        expect(resp.status).toBe(200);
+      });
     });
   });
 });
---
+++ modules/authentication-api/src/modules/identity/tests/mock.ts
@@ -0,0 +1,15 @@
+export const CREATE_IDENTITY_MOCK = {
+  test_mnemonic:
+    'radar laundry matter way sweet arrive hundred behind tribe phrase cave rely',
+  did_uri: '',
+  email: 'luka.golinar@kalmia.si',
+  did_create_op_data: {
+    encoded:
+      '0xdc1204ee2b2e274bc74b744264ec67b9a13a4366bf8118bb217cac90f634e524a2bee528b67f4a1500c5223b1c52bc6950ea9397f3baebe6dd2e26b5940804240400e101a638d8b54a788bf7128b099cd184868aa2231e29c400b4ab4f7926748138000004000000',
+    encodedSignature:
+      '0x6a71ca26685a311e773e85d9c022ef265e63dc5a6f7b8cd86cc36f5f2d86a7743e9d0caba8cb8ca953a4e50def61ad49b125150ab742c1300cd4b45fef90ed80 ',
+  },
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imx1a2EuZ29saW5hckBrYWxtaWEuc2kiLCJpYXQiOjE2NzI4NDczMjUsImV4cCI6MTY3MjkzMzcyNSwic3ViIjoiSURFTlRJVFlfRU1BSUxfVkVSSUZJQ0FUSU9OIn0.tKZ9boqW3sYSLKkcLui8vUBcDeV8K801gZrwc_iMzc8',
+  credential: { someprops: 'I am a credential lalala' },
+};
