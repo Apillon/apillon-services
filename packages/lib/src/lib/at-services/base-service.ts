2591ce54809bc1ba770687f6f759b8ee7941ae5a
Tine Mlakar <tine.mlakar@kalmia.si>
2023-01-10 10:13:39 +0100

    additional logging

--- packages/lib/src/lib/at-services/base-service.ts
+++ packages/lib/src/lib/at-services/base-service.ts
@@ -70,6 +70,11 @@ export abstract class BaseService {
       }
     }
     console.log(result);
+    console.log(isAsync);
+    console.log(result?.FunctionError);
+    console.log(result?.success);
+    console.log(result?.FunctionError || !result?.success);
+    console.log(!isAsync && (result?.FunctionError || !result?.success));
 
     if (!isAsync && (result?.FunctionError || !result?.success)) {
       // CodeException causes circular dependency!
