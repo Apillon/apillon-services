797d0ad941e356f6c9142e09ef27156a9c99f2d3
Tine Mlakar <tine.mlakar@kalmia.si>
2023-01-17 14:58:48 +0100

    fix build configs

--- bin/codebuild/dev/create-build-projects-dev.bat
+++ bin/codebuild/dev/create-build-projects-dev.bat
@@ -1,7 +1,7 @@
 @REM aws codebuild create-project --cli-input-json file://dev-console-config-dev.json
 @REM aws codebuild create-webhook --cli-input-json file://dev-console-webhook-dev.json
 
-@REM aws codebuild create-project --cli-input-json file://access-config-dev.json
+aws codebuild create-project --cli-input-json file://access-config-dev.json
 aws codebuild create-webhook --cli-input-json file://access-webhook-dev.json
 
 aws codebuild create-project --cli-input-json file://monitoring-config-dev.json
--- bin/sqs/create-all.sh
+++ bin/sqs/create-all.sh
@@ -1,8 +1,8 @@
 aws sqs create-queue --queue-name apillon-storage-queue-dev-dlq --attributes file://dev/storage-queue-dev-dlq.json --region eu-west-1
 aws sqs create-queue --queue-name apillon-storage-queue-dev --attributes file://dev/storage-queue-dev.json  --region eu-west-1
 
-aws sqs create-queue --queue-name apillon-authentication-queue-dev-dlq --attributes file://dev/authentication-queue-dev-dlq.json --region eu-west-1
-aws sqs create-queue --queue-name apillon-authentication-queue-dev --attributes file://dev/authentication-queue-dev.json  --region eu-west-1
+aws sqs create-queue --queue-name apillon-auth-queue-dev-dlq --attributes file://dev/auth-queue-dev-dlq.json --region eu-west-1
+aws sqs create-queue --queue-name apillon-auth-queue-dev --attributes file://dev/auth-queue-dev.json  --region eu-west-1
 
 aws sqs create-queue --queue-name apillon-storage-queue-staging-dlq --attributes file://staging/storage-queue-staging-dlq.json  --region eu-west-1
 aws sqs create-queue --queue-name apillon-storage-queue-staging --attributes file://staging/storage-queue-staging.json  --region eu-west-1
