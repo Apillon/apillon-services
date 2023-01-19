559318631beddb88e9a5db64859754c337c1d188
Luka Golinar <luka.golinar@kalmia.si>
2023-01-19 09:08:44 +0100

    feat: Jwt guard

---
+++ modules/authentication-api/src/modules/identity/dtos/identity-email.dto.ts
@@ -0,0 +1,23 @@
+import { ModelBase, PopulateFrom } from '@apillon/lib';
+import { prop } from '@rawmodel/core';
+import { stringParser } from '@rawmodel/parsers';
+import { presenceValidator, emailValidator } from '@rawmodel/validators';
+import { AuthenticationErrorCode } from '../../../config/types';
+
+export class AttestationEmailDto extends ModelBase {
+  @prop({
+    parser: { resolver: stringParser() },
+    populatable: [PopulateFrom.PROFILE],
+    validators: [
+      {
+        resolver: presenceValidator(),
+        code: AuthenticationErrorCode.USER_EMAIL_NOT_PRESENT,
+      },
+      {
+        resolver: emailValidator(),
+        code: AuthenticationErrorCode.USER_EMAIL_NOT_VALID,
+      },
+    ],
+  })
+  public email: string;
+}
