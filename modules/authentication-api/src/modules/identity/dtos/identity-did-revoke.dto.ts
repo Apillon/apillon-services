116b67bbf53b187f52c969e651daa694e0a5c891
Luka Golinar <luka.golinar@kalmia.si>
2023-01-20 14:43:20 +0100

    feat: identity-did-revoke.dto.ts, third time's the charm

---
+++ modules/authentication-api/src/modules/identity/dtos/identity-did-revoke.dto.ts
@@ -0,0 +1,47 @@
+import { ModelBase, PopulateFrom } from '@apillon/lib';
+import { DidUri } from '@kiltprotocol/types';
+import { prop } from '@rawmodel/core';
+import { stringParser } from '@rawmodel/parsers';
+import { presenceValidator, emailValidator } from '@rawmodel/validators';
+import { AuthenticationErrorCode } from '../../../config/types';
+import { didUriValidator } from '../validators/did-create.validator';
+
+export class IdentityDidRevokeDto extends ModelBase {
+  @prop({
+    parser: { resolver: stringParser() },
+    populatable: [PopulateFrom.PROFILE],
+    validators: [
+      {
+        resolver: presenceValidator(),
+        code: AuthenticationErrorCode.USER_EMAIL_NOT_PRESENT,
+      },
+      {
+        resolver: emailValidator(),
+        code: AuthenticationErrorCode.USER_EMAIL_NOT_VALID,
+      },
+    ],
+  })
+  public email: string;
+
+  @prop({
+    parser: { resolver: stringParser() },
+    populatable: [PopulateFrom.PROFILE],
+    validators: [
+      {
+        resolver: presenceValidator(),
+        code: AuthenticationErrorCode.DID_URI_NOT_PRESENT,
+      },
+      {
+        resolver: didUriValidator(),
+        code: AuthenticationErrorCode.DID_URI_INVALID,
+      },
+    ],
+  })
+  public didUri: string;
+
+  @prop({
+    parser: { resolver: stringParser() },
+    populatable: [PopulateFrom.PROFILE],
+  })
+  public token: string;
+}
