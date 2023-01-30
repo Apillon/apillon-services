e30e447ea877bf391408c331e77908cc5c660891
Luka Golinar <luka.golinar@kalmia.si>
2023-01-30 16:51:43 +0100

    feat: Barebone service methods

--- modules/authentication-api/src/modules/sporran/dtos/sporran.controller.ts
+++ modules/authentication-api/src/modules/sporran/dtos/sporran.controller.ts
@@ -1,18 +1,20 @@
 import { Ctx } from '@apillon/modules-lib';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { AuthenticationApiContext } from '../../../context';
 import { SporranService } from './sporran.service';
 
 @Controller('sporran')
 export class SporranController {
-  constructor(private identityService: SporranService) {}
+  constructor(private sporranService: SporranService) {}
 
-  @Get('challenge')
-  async sporranGetChallange(
-    @Ctx() context: AuthenticationApiContext,
-    @Query('email') email: string,
-  ) {
-    return randomUUID();
+  @Get('session/values')
+  async sporranGetChallange(@Ctx() context: AuthenticationApiContext) {
+    return await this.sporranService.getSessionValues(context);
+  }
+
+  @Post('session/verify-challenge')
+  async sporranVerifySession(@Ctx() context: AuthenticationApiContext) {
+    return await this.sporranService.getSessionValues(context);
   }
 }
--- modules/authentication-api/src/modules/sporran/dtos/sporran.service.ts
+++ modules/authentication-api/src/modules/sporran/dtos/sporran.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class SporranService {}
+export class SporranService {
+  async getSessionValues(
+    context: AuthenticationApiContext,
+    body: AttestationEmailDto,
+  ): Promise<any> {
+    const challenge = randomUUID();
+    return {
+      challenge: challenge,
+    };
+  }
+
+  async verifySession(
+    context: AuthenticationApiContext,
+    body: AttestationEmailDto,
+  ): Promise<any> {
+    const challenge = randomUUID();
+    return true;
+  }
+}
