edd1baa875218be7705f41c9d1160de3ade24e5c
Vinko Šmid <vinkosmid@gmail.com>
2023-02-02 15:04:54 +0100

    overwrite file in storage bucket fix

--- services/storage/src/modules/storage/models/file.model.ts
+++ services/storage/src/modules/storage/models/file.model.ts
@@ -61,6 +61,7 @@ export class File extends AdvancedSQLModel {
     ],
     serializable: [
       SerializeFor.INSERT_DB,
+      SerializeFor.UPDATE_DB,
       SerializeFor.ADMIN,
       SerializeFor.SERVICE,
       SerializeFor.PROFILE,
