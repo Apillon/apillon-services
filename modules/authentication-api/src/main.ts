00e99e80f1ac62a503a3486bed70f938d2b8b6a7
Luka Golinar <luka.golinar@kalmia.si>
2023-02-15 10:34:04 +0100

    feat: Removed unused database populatables, remvoed processors from modules

--- modules/authentication-api/src/main.ts
+++ modules/authentication-api/src/main.ts
@@ -2,9 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExceptionsFilter, ResponseInterceptor } from '@apillon/modules-lib';
 import { env } from '@apillon/lib';
-import type { Context } from 'aws-lambda/handler';
-import { IdentityService } from './modules/identity/identity.service';
-import { IdentityEventType } from './config/types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -17,13 +14,3 @@ async function bootstrap() {
 }
 
 bootstrap().catch((err) => console.error(err.message));
-
-// Worker event processor
-export async function processEvent(event, context: Context): Promise<any> {
-  const processors = {
-    [IdentityEventType.CREATE_DECENTRALIZED_IDENTITY]: new IdentityService()
-      .generateIdentity,
-  };
-
-  return await processors[event.eventName](event, context);
-}
