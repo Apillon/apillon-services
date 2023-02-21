f1349d08fd966308c8e139748d415f880d1cdc1c
Vinko Šmid <vinko.smid@kalmia.si>
2023-02-21 07:22:28 +0100

    list collection transactions

--- services/nfts/src/modules/transaction/transaction.service.ts
+++ services/nfts/src/modules/transaction/transaction.service.ts
@@ -5,8 +5,12 @@ import {
   SerializeFor,
   TransactionQueryFilter,
 } from '@apillon/lib';
+import { DbTables, NftsErrorCode } from '../../config/types';
 import { ServiceContext } from '../../context';
-import { NftsValidationException } from '../../lib/exceptions';
+import {
+  NftsCodeException,
+  NftsValidationException,
+} from '../../lib/exceptions';
 import { executeTransactionStatusWorker } from '../../scripts/serverless-workers/execute-transaction-status-worker';
 import { Collection } from '../nfts/models/collection.model';
 import { Transaction } from './models/transaction.model';
@@ -44,8 +48,25 @@ export class TransactionService {
     event: { collection_uuid: string; query: TransactionQueryFilter },
     context: ServiceContext,
   ) {
-    return await new Transaction({}, context).getList(
-      new TransactionQueryFilter(event.query),
-    );
+    const collection: Collection = await new Collection(
+      {},
+      context,
+    ).populateByUUID(event.collection_uuid);
+    if (!collection.exists()) {
+      throw new NftsCodeException({
+        status: 500,
+        code: NftsErrorCode.NFT_CONTRACT_OWNER_ERROR,
+        context: context,
+      });
+    }
+    collection.canAccess(context);
+
+    const query = new TransactionQueryFilter(event.query);
+    query.populate({
+      refTable: DbTables.COLLECTION,
+      refId: collection.id,
+    });
+
+    return await new Transaction({}, context).getList(query);
   }
 }
