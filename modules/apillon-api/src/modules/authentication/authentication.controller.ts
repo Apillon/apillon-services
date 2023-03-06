161fedee9d66a2eb41aa989296eaa74eee52a89c
Luka Golinar <luka.golinar@kalmia.si>
2023-03-06 12:14:17 +0100

    feat: PR changes

--- modules/apillon-api/src/modules/authentication/authentication.controller.ts
+++ modules/apillon-api/src/modules/authentication/authentication.controller.ts
@@ -1,21 +1,36 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
-import { Ctx, Validation } from '@apillon/modules-lib';
+import { ApiKeyPermissions, Ctx, Validation } from '@apillon/modules-lib';
 import { ApillonApiContext } from '../../context';
 import { AuthService } from './authentication.service';
 import { ValidationGuard } from '../../guards/validation.guard';
-import { ValidateFor } from '@apillon/lib';
+import {
+  AttachedServiceType,
+  DefaultApiKeyRole,
+  ValidateFor,
+} from '@apillon/lib';
 import { VerifyLoginDto } from '@apillon/lib';
+import { AuthGuard } from '../../guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Get('session-token')
+  @ApiKeyPermissions({
+    role: DefaultApiKeyRole.KEY_EXECUTE,
+    serviceType: AttachedServiceType.AUTHENTICATION,
+  })
+  @UseGuards(AuthGuard)
   async generateSessionToken(@Ctx() context: ApillonApiContext) {
     return await this.authService.generateSessionToken(context);
   }
 
   @Get('verify-login')
+  @ApiKeyPermissions({
+    role: DefaultApiKeyRole.KEY_EXECUTE,
+    serviceType: AttachedServiceType.AUTHENTICATION,
+  })
+  @UseGuards(AuthGuard)
   @Validation({
     dto: VerifyLoginDto,
     validateFor: ValidateFor.QUERY,
