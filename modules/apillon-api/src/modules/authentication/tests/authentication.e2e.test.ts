9816c24c5e34b0c669e5dc7b5cb99d59111b6125
Luka Golinar <luka.golinar@kalmia.si>
2023-03-06 15:00:25 +0100

    feat: TEst directory

---
+++ modules/apillon-api/src/modules/authentication/tests/authentication.e2e.test.ts
@@ -0,0 +1,76 @@
+import {
+  ApiKeyRoleBaseDto,
+  DefaultApiKeyRole,
+  AttachedServiceType,
+} from '@apillon/lib';
+import {
+  Stage,
+  releaseStage,
+  createTestUser,
+  createTestApiKey,
+  createTestProject,
+  createTestProjectService,
+} from '@apillon/tests-lib';
+import { setupTest } from '../../../../test/helpers/setup';
+import * as request from 'supertest';
+
+describe('Authentication tests', () => {
+  let stage: Stage;
+  let testUser;
+  let apiKey;
+  let testProject;
+  let testService;
+
+  beforeAll(async () => {
+    stage = await setupTest();
+
+    testUser = await createTestUser(stage.devConsoleContext, stage.amsContext);
+    testProject = await createTestProject(testUser, stage.devConsoleContext);
+    testService = await createTestProjectService(
+      stage.devConsoleContext,
+      testProject,
+    );
+
+    apiKey = await createTestApiKey(stage.amsContext, testProject.project_uuid);
+    await apiKey.assignRole(
+      new ApiKeyRoleBaseDto().populate({
+        role_id: DefaultApiKeyRole.KEY_EXECUTE,
+        project_uuid: testProject.project_uuid,
+        service_uuid: testService.service_uuid,
+        serviceType_id: AttachedServiceType.AUTHENTICATION,
+      }),
+    );
+    await apiKey.assignRole(
+      new ApiKeyRoleBaseDto().populate({
+        role_id: DefaultApiKeyRole.KEY_READ,
+        project_uuid: testProject.project_uuid,
+        service_uuid: testService.service_uuid,
+        serviceType_id: AttachedServiceType.AUTHENTICATION,
+      }),
+    );
+    await apiKey.assignRole(
+      new ApiKeyRoleBaseDto().populate({
+        role_id: DefaultApiKeyRole.KEY_WRITE,
+        project_uuid: testProject.project_uuid,
+        service_uuid: testService.service_uuid,
+        serviceType_id: AttachedServiceType.AUTHENTICATION,
+      }),
+    );
+  });
+
+  afterAll(async () => {
+    await releaseStage(stage);
+  });
+
+  test('Anyone with an api-key can generate a session token ', async () => {
+    const response = await request(stage.http)
+      .get(`/auth/generate-session`)
+      .set(
+        'Authorization',
+        `Basic ${Buffer.from(
+          apiKey.apiKey + ':' + apiKey.apiKeySecret,
+        ).toString('base64')}`,
+      );
+    expect(response.status).toBe(201);
+  });
+});
