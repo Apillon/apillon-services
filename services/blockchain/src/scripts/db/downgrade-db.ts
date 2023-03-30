d0ae309f300b0b72cc400e43a3b1715dbe63c067
Tadej Vengust <vengust.tadej@gmail.com>
2023-03-30 15:18:03 +0200

    Update db scripts.

--- services/blockchain/src/scripts/db/downgrade-db.ts
+++ services/blockchain/src/scripts/db/downgrade-db.ts
@@ -1,45 +1,25 @@
-import * as readline from 'readline';
-import { env, downgradeDatabase } from '@apillon/lib';
+import { env, getEnvSecrets, SqlMigrator } from '@apillon/lib';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+async function run() {
+  await getEnvSecrets();
 
-let steps = 1;
+  const migrator = new SqlMigrator({
+    database: env.BLOCKCHAIN_MYSQL_DATABASE,
+    host: env.BLOCKCHAIN_MYSQL_HOST,
+    port: env.BLOCKCHAIN_MYSQL_PORT,
+    user: env.BLOCKCHAIN_MYSQL_USER,
+    password: env.BLOCKCHAIN_MYSQL_PASSWORD,
+  });
 
-const run = async () => {
-  await downgradeDatabase(
-    env.BLOCKCHAIN_MYSQL_DATABASE,
-    env.BLOCKCHAIN_MYSQL_HOST,
-    env.BLOCKCHAIN_MYSQL_PORT,
-    env.BLOCKCHAIN_MYSQL_USER,
-    env.BLOCKCHAIN_MYSQL_PASSWORD,
-    steps,
-  );
-};
+  const showDialog = !process.argv.includes('--F');
+  await migrator.downgrade(showDialog);
+}
 
-rl.question(
-  `You are about to downgrade database ${env.BLOCKCHAIN_MYSQL_DATABASE} @ ${env.BLOCKCHAIN_MYSQL_HOST}.\n Set number of versions to downgrade (-1 for all, 0 to exit):`,
-  (answer) => {
-    steps = parseInt(answer);
-    if (steps) {
-      console.log(`Downgrading ${steps > 0 ? steps : 'ALL'} version(s).`);
-    } else {
-      console.log('Invalid input. Exiting.');
-      process.exit(0);
-    }
-
-    rl.close();
-
-    run()
-      .then(() => {
-        console.log('Complete!');
-        process.exit(0);
-      })
-      .catch((err) => {
-        console.log(err);
-        process.exit(1);
-      });
-  },
-);
+run()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
--- services/blockchain/src/scripts/db/upgrade-db.ts
+++ services/blockchain/src/scripts/db/upgrade-db.ts
@@ -1,53 +1,25 @@
-import * as readline from 'readline';
-import { bgYellow, black } from 'colors/safe';
-import { env, upgradeDatabase } from '@apillon/lib';
+import { env, getEnvSecrets, SqlMigrator } from '@apillon/lib';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+async function run() {
+  await getEnvSecrets();
 
-let steps = 0;
+  const migrator = new SqlMigrator({
+    database: env.BLOCKCHAIN_MYSQL_DATABASE,
+    host: env.BLOCKCHAIN_MYSQL_HOST,
+    port: env.BLOCKCHAIN_MYSQL_PORT,
+    user: env.BLOCKCHAIN_MYSQL_USER,
+    password: env.BLOCKCHAIN_MYSQL_PASSWORD,
+  });
 
-const run = async () => {
-  await upgradeDatabase(
-    env.BLOCKCHAIN_MYSQL_DATABASE,
-    env.BLOCKCHAIN_MYSQL_HOST,
-    env.BLOCKCHAIN_MYSQL_PORT,
-    env.BLOCKCHAIN_MYSQL_USER,
-    env.BLOCKCHAIN_MYSQL_PASSWORD,
-    steps,
-  );
-};
+  const showDialog = !process.argv.includes('--F');
+  await migrator.upgrade(showDialog);
+}
 
-rl.question(
-  `You are about to upgrade database ${bgYellow(
-    black(` ${env.BLOCKCHAIN_MYSQL_DATABASE} @ ${env.BLOCKCHAIN_MYSQL_HOST} `),
-  )}.
-
-Set number of versions to upgrade ('Y' for all, '<number>' for number of versions, 'N' to exit):`,
-  (answer) => {
-    steps = parseInt(answer);
-    if (answer.toUpperCase() === 'Y') {
-      steps = 0;
-      console.log('Upgrading to LATEST version.');
-    } else if (steps) {
-      console.log(`Upgrading for ${steps} version(s).`);
-    } else {
-      console.log('Invalid input. Exiting.');
-      process.exit(0);
-    }
-
-    rl.close();
-
-    run()
-      .then(() => {
-        console.log('Complete!');
-        process.exit(0);
-      })
-      .catch((err) => {
-        console.log(err);
-        process.exit(1);
-      });
-  },
-);
+run()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
