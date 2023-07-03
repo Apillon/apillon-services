1778a6b3bde5e15f5693e349cc9c6c41b5a3f757
Luka Golinar <luka.golinar@kalmia.si>
2023-07-03 15:28:35 +0200

    transaction dto

---
+++ services/authentication/src/migration-scripts/migrations/4-transaction.ts
@@ -0,0 +1,30 @@
+import { DbTables } from '../../config/types';
+
+export async function upgrade(
+  queryFn: (query: string, values?: any[]) => Promise<any[]>,
+): Promise<void> {
+  await queryFn(`
+    CREATE TABLE IF NOT EXISTS \`${DbTables.TRANSACTION}\` (
+      \`id\` INT NOT NULL AUTO_INCREMENT,
+      \`transactionHash\` VARCHAR(500) NOT NULL,
+      \`transactionType\` VARCHAR(50) NULL,
+      \`transactionStatus\` INT NULL,
+      \`refTable\` VARCHAR(100) NULL,
+      \`refId\` INT NULL,
+      \`status\` INT NULL,
+      \`createTime\` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+      \`createUser\` INT NULL,
+      \`updateTime\` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      \`updateUser\` INT NULL,
+      PRIMARY KEY (\`id\`)
+    );
+  `);
+}
+
+export async function downgrade(
+  queryFn: (query: string, values?: any[]) => Promise<any[]>,
+): Promise<void> {
+  await queryFn(`
+    DROP TABLE IF EXISTS \`${DbTables.TRANSACTION}\`;
+  `);
+}
