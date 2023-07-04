edefada2c7c4e1044978692183c18fedefd6edb1
Luka Golinar <luka.golinar@kalmia.si>
2023-07-04 14:09:55 +0200

    feat: Scripts, dev worker.

---
+++ packages/workers-lib/src/lib/dev-worker.ts
@@ -0,0 +1,29 @@
+import { QueueWorkerType } from '../config/types';
+import {
+  ServiceDefinition,
+  ServiceDefinitionType,
+  WorkerDefinition,
+} from './serverless-workers';
+
+export async function callDevWorker(
+  workerName: string,
+  workerClass: any,
+  parameters?: any,
+): Promise<void> {
+  console.log(`Starting DEV ${workerName} worker ...`);
+
+  // Directly calls Kilt worker -> USED ONLY FOR DEVELOPMENT!!
+  const serviceDef: ServiceDefinition = {
+    type: ServiceDefinitionType.SQS,
+    config: { region: 'test' },
+    params: { FunctionName: 'test' },
+  };
+
+  const wd = new WorkerDefinition(serviceDef, workerName, {
+    parameters,
+  });
+
+  const worker = new workerClass(wd, context, QueueWorkerType.EXECUTOR);
+
+  await worker.runExecutor(parameters);
+}
