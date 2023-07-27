afa3c29ab9e68b21780486ae3abfac8f73e3e738
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2023-07-27 12:05:00 +0200

    feat: Remove project_uuid property from ProjectAccessModel

--- packages/lib/src/lib/base-models/project-access.model.ts
+++ packages/lib/src/lib/base-models/project-access.model.ts
@@ -4,17 +4,35 @@ import { CodeException } from '../exceptions/exceptions';
 import { AdvancedSQLModel } from './advanced-sql.model';
 import { HttpStatus } from '@nestjs/common';
 
+/**
+ * Class which contains access control methods for a project.
+ * Contains canAccess and canModify methods which check if a project can be accessed and/or modified.
+ * @abstract
+ * @class ProjectAccessModel
+ * @typedef {ProjectAccessModel}
+ * @extends {AdvancedSQLModel}
+ */
 export abstract class ProjectAccessModel extends AdvancedSQLModel {
-  public project_uuid: string;
-
+  /**
+   * Method which checks if a project can be accessed depending on the user role.
+   * project_uuid is optional if the inheriting class defines that property
+   * @param {Context} context
+   * @param {string} [project_uuid=null]
+   * @returns {(boolean | Promise<boolean>)}
+   */
   public canAccess(
     context: Context,
-    project_uuid = this.project_uuid,
+    project_uuid: string = null,
   ): boolean | Promise<boolean> {
     // Admins are allowed to access items on any project
     if (context.user.userRoles.includes(DefaultUserRole.ADMIN)) {
       return true;
     }
+
+    if (this['project_uuid']) {
+      project_uuid = this['project_uuid'];
+    }
+
     if (
       !context.hasRoleOnProject(
         [
@@ -34,10 +52,20 @@ export abstract class ProjectAccessModel extends AdvancedSQLModel {
     }
   }
 
+  /**
+   * Method which checks if a project can be modified depending on the user role.
+   * project_uuid is optional if the inheriting class defines that property
+   * @param {Context} context
+   * @param {string} [project_uuid=null]
+   * @returns {(boolean | Promise<boolean>)}
+   */
   public canModify(
     context: Context,
-    project_uuid = this.project_uuid,
+    project_uuid: string = null,
   ): boolean | Promise<boolean> {
+    if (this['project_uuid']) {
+      project_uuid = this['project_uuid'];
+    }
     if (
       !context.hasRoleOnProject(
         [
