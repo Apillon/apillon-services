312c0280e40e87cdd4d47dbcfe7d8be4ac8cd5bf
Luka Golinar <luka.golinar@kalmia.si>
2023-08-09 13:30:49 +0200

    feat: Moved env var outside quotes

--- services/blockchain/src/config/substrate-parachain.ts
+++ services/blockchain/src/config/substrate-parachain.ts
@@ -2,19 +2,20 @@ import { SubstrateChain, env } from '@apillon/lib';
 import { CrustBlockchainIndexer } from '../modules/blockchain-indexers/substrate/crust/crust-indexer.service';
 import { KiltBlockchainIndexer } from '../modules/blockchain-indexers/substrate/kilt/kilt-indexer.service';
 
+// TODO: Maybe move worker name to env, so it can be configurable
 export const ParachainConfig = {
   CRUST: {
     id: SubstrateChain.CRUST,
     name: 'CRUST',
     webhookWorkerName: 'UpdateCrustStatusWorker',
-    sqsUrl: `${env.AUTH_AWS_WORKER_SQS_URL}`,
+    sqsUrl: env.AUTH_AWS_WORKER_SQS_URL,
     indexer: CrustBlockchainIndexer,
   },
   KILT: {
     id: SubstrateChain.KILT,
     name: 'KILT',
     webhookWorkerName: 'UpdateStateWorker',
-    sqsUrl: `${env.AUTH_AWS_WORKER_SQS_URL}`,
+    sqsUrl: env.AUTH_AWS_WORKER_SQS_URL,
     indexer: KiltBlockchainIndexer,
   },
 };
