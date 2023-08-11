6299443400ece9f45932937266ba345efd2f4971
Tine Mlakar <tine.mlakar@kalmia.si>
2023-08-11 10:32:29 +0200

    fix seed script

--- services/storage/src/migration-scripts/seeds/1-Republish-Ipns-job.ts
+++ services/storage/src/migration-scripts/seeds/1-Republish-Ipns-job.ts
@@ -3,7 +3,7 @@ export async function upgrade(
 ): Promise<void> {
   await queryFn(`
     INSERT INTO \`job\` (\`status\`, \`name\`, \`interval\`, \`nextRun\`, \`timeout\`) 
-    VALUES ('5', 'RepublishIpnsWorker', '* */1 * * *', NOW(), '3600'); `);
+    VALUES ('5', 'RepublishIpnsWorker', '0 */1 * * *', NOW(), '3600'); `);
 }
 
 export async function downgrade(
