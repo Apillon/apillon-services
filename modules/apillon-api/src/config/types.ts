1fdd1bbafb9a08ae1a0344b730869a951f310491
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2023-08-30 12:56:13 +0200

    Add type number separators

--- modules/apillon-api/src/config/types.ts
+++ modules/apillon-api/src/config/types.ts
@@ -1,17 +1,17 @@
 export enum DbTables {}
 
 /**
- * Validation error codes - 42205000.
+ * Validation error codes - 422_05_000.
  */
 export enum ValidatorErrorCode {
-  DEFAULT_VALIDATION_ERROR = 42205000,
+  DEFAULT_VALIDATION_ERROR = 422_05_000,
 }
 
 /**
  * Resource not found error codes - 404000.
  */
 export enum ResourceNotFoundErrorCode {
-  DEFAULT_RESOURCE_NOT_FOUND_ERROR = 40405000,
+  DEFAULT_RESOURCE_NOT_FOUND_ERROR = 404_05_000,
 }
 
 /**
