fe1bed708d4109fa1afdd9144c75f4d7aee0e1e9
Vinko Šmid <vinkos.smid@kalmia.si>
2023-09-12 10:08:13 +0200

    Error handling for RepublishIpnsWorker executor.

--- services/storage/src/workers/republish-ipns-worker.ts
+++ services/storage/src/workers/republish-ipns-worker.ts
@@ -6,6 +6,7 @@ import {
   WorkerLogStatus,
 } from '@apillon/workers-lib';
 import axios from 'axios';
+import { IPFSService } from '../modules/ipfs/ipfs.service';
 
 export class RepublishIpnsWorker extends BaseQueueWorker {
   public constructor(
@@ -52,7 +53,17 @@ export class RepublishIpnsWorker extends BaseQueueWorker {
         );
         console.info('IPNS republished', item);
       } catch (error) {
-        console.error('Error republishing IPNS', error, item);
+        console.error('Error republishing IPNS.', error, item);
+        if (error.data.Message === 'no key by the given name was found') {
+          try {
+            console.info(
+              'Calling IPFSService.publishToIPNS so that new key will be generated...',
+            );
+            await IPFSService.publishToIPNS(item.cid, item.keyName);
+          } catch (error2) {
+            console.error('Error in IPFSService.publishToIPNS', error2);
+          }
+        }
       }
     }
   }
