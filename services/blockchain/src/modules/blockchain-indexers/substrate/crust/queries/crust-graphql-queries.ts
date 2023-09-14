e7af128926ce536cae6cf85298b183c202c1720d
Tine Mlakar <tine.mlakar@kalmia.si>
2023-09-14 16:40:11 +0200

    gql query fixed

--- services/blockchain/src/modules/blockchain-indexers/substrate/crust/queries/crust-graphql-queries.ts
+++ services/blockchain/src/modules/blockchain-indexers/substrate/crust/queries/crust-graphql-queries.ts
@@ -191,17 +191,22 @@ export class CrustGQLQueries {
       storageOrders(
         where: {
           extrinsicHash_eq: $extrinsicHash
-          account: $address
+          account_eq: $address
         }
       ) {
         account
         extrinsicHash
       }
       transfers(
-        where: { extrinsicHash_eq: $extrinsicHash, from: $address }
+        where: { extrinsicHash_eq: $extrinsicHash, from_eq: $address }
       ) {
         extrinsicHash
         from
+      }
+      systems(
+        where: { account_eq: $address , extrinsicHash_eq: $extrinsicHash }
+      ) {
+        extrinsicHash
       }
     }
   `;
