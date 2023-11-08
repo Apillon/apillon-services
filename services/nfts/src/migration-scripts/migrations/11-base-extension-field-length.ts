73e7881c38888e45a0cdf1057c19a8fef321b698
Vinko Šmid <vinko.smid@kalmia.si>
2023-11-08 23:32:52 +0100

    slash added to the end of ipfs links. Change length of base extension field.

---
+++ services/nfts/src/migration-scripts/migrations/11-base-extension-field-length.ts
@@ -0,0 +1,18 @@
+import { DbTables } from '../../config/types';
+
+export async function upgrade(
+  queryFn: (query: string, values?: any[]) => Promise<any[]>,
+): Promise<void> {
+  await queryFn(`
+    ALTER TABLE \`${DbTables.COLLECTION}\`
+    MODIFY COLUMN \`baseExtension\` VARCHAR(255) NOT NULL;
+    `);
+}
+
+export async function downgrade(
+  queryFn: (query: string, values?: any[]) => Promise<any[]>,
+): Promise<void> {
+  await queryFn(`
+        ALTER TABLE \`${DbTables.TRANSACTION}\` DROP COLUMN transaction_uuid;
+    `);
+}
