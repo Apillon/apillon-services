9898a6f76dad5d24f2fbdf4bdddac57e37c30ec5
Tadej <tadej.dev@gmail.com>
2024-01-15 11:23:57 +0000

    fix(computing): add missing presence validators

--- packages/lib/src/lib/at-services/computing/dtos/encrypt-content.dto.ts
+++ packages/lib/src/lib/at-services/computing/dtos/encrypt-content.dto.ts
@@ -1,7 +1,8 @@
 import { prop } from '@rawmodel/core';
 import { stringParser } from '@rawmodel/parsers';
-import { PopulateFrom } from '../../../../config/types';
+import { PopulateFrom, ValidatorErrorCode } from '../../../../config/types';
 import { ModelBase } from '../../../base-models/base';
+import { presenceValidator } from '../../../validators';
 
 export class EncryptContentDto extends ModelBase {
   @prop({
@@ -14,7 +15,12 @@ export class EncryptContentDto extends ModelBase {
   @prop({
     parser: { resolver: stringParser() },
     populatable: [PopulateFrom.PROFILE, PopulateFrom.ADMIN],
-    validators: [],
+    validators: [
+      {
+        resolver: presenceValidator(),
+        code: ValidatorErrorCode.REQUIRED_DATA_NOT_PRESENT,
+      },
+    ],
   })
   public content: string;
 }
