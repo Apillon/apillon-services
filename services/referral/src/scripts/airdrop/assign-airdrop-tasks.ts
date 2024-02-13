3f3097668e74a1216cffa744d5b40e694c153526
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2024-02-13 15:47:54 +0100

    feat: Add mongo logic to check user API calls

--- services/referral/src/scripts/airdrop/assign-airdrop-tasks.ts
+++ services/referral/src/scripts/airdrop/assign-airdrop-tasks.ts
@@ -1,9 +1,17 @@
-import { Mongo, MySql, SerializeFor, SqlModelStatus, env } from '@apillon/lib';
+import {
+  Mongo,
+  MongoCollections,
+  MySql,
+  SerializeFor,
+  SqlModelStatus,
+  env,
+} from '@apillon/lib';
 import { AirdropTask } from '../../modules/airdrop/models/airdrop-task.model';
 import { ServiceContext } from '@apillon/service-lib';
 import { Player } from '../../modules/referral/models/player.model';
 
 let referralContext: ServiceContext;
+let mongo: Mongo;
 
 async function setupDatabases() {
   const devConsoleSql = new MySql({
@@ -15,21 +23,21 @@ async function setupDatabases() {
   });
   await devConsoleSql.connect();
 
-  const amsSql = new MySql({
+  const accessSql = new MySql({
     host: env.ACCESS_MYSQL_HOST,
     database: env.ACCESS_MYSQL_DATABASE,
     password: env.ACCESS_MYSQL_PASSWORD,
     port: env.ACCESS_MYSQL_PORT,
     user: env.ACCESS_MYSQL_USER,
   });
-  await amsSql.connect();
+  await accessSql.connect();
 
-  const lmasMongo = new Mongo(
+  mongo = new Mongo(
     env.MONITORING_MONGO_SRV,
     env.MONITORING_MONGO_DATABASE,
     10,
   );
-  await lmasMongo.connect();
+  await mongo.connect();
 
   const storageSql = new MySql({
     host: env.STORAGE_MYSQL_HOST,
@@ -108,8 +116,8 @@ async function setupDatabases() {
 
   return {
     devConsoleSql,
-    amsSql,
-    lmasMongo,
+    accessSql,
+    mongo,
     storageSql,
     configSql,
     referralSql,
@@ -134,6 +142,7 @@ let creditTransactions: any[];
 let spaces: any[];
 let computingContracts: any[];
 let identities: any[];
+let apiKeys: any[];
 // let referrals: any[];
 
 async function main() {
@@ -177,6 +186,9 @@ async function main() {
   );
   identities = await databases.authSql.paramExecute(
     `SELECT * FROM identity WHERE state = 'attested' AND status = ${SqlModelStatus.ACTIVE}`,
+  );
+  apiKeys = await databases.accessSql.paramExecute(
+    `SELECT * FROM apiKey WHERE status = ${SqlModelStatus.ACTIVE}`,
   );
   // referrals = await databases.referralSql.paramExecute(
   //   `SELECT * FROM ${DbTables.PLAYER}`,
@@ -234,6 +246,7 @@ async function assignAirdropTasks(user: any): Promise<AirdropTask> {
     creditsSpent: own(creditTransactions)
       .filter((p) => p.direction == 2)
       .reduce((total, current) => total + current.amount, 0),
+    ...(await assignApiTasks(own(apiKeys).map((a) => a.apiKey))),
   });
 
   for (const [task, isCompleted] of Object.entries(
@@ -269,6 +282,22 @@ async function assignReferredUsers(user: any, airdropTasks: AirdropTask[]) {
   }
 }
 
+async function assignApiTasks(apiKeys: any[]) {
+  const collection = mongo.db.collection(MongoCollections.API_REQUEST_LOGS);
+
+  const checkApiCalled = ($regex: RegExp) =>
+    collection
+      .count({ apiKey: { $in: apiKeys }, url: { $regex, $options: 'i' } })
+      .then((c) => c > 0);
+
+  return {
+    websiteUploadedApi: await checkApiCalled(/^\/hosting.*upload/),
+    identitySdkUsed: await checkApiCalled(/^\/wallet-identity.*$/),
+    fileUploadedApi: await checkApiCalled(/^\/storage\/buckets.*upload/),
+    nftMintedApi: await checkApiCalled(/^\/nfts\/collections.*mint/),
+  };
+}
+
 async function saveAirdropTasks(airdropTasks: AirdropTask) {
   await airdropTasks.saveOrUpdate();
   console.info(`Assigned airdrop tasks for user ${airdropTasks.user_uuid}`);
@@ -291,6 +320,10 @@ const taskPoints = {
   kiltIdentityCreated: 10,
   collaboratorAdded: 1,
   usersReferred: 2,
+  websiteUploadedApi: 5,
+  identitySdkUsed: 2,
+  fileUploadedApi: 5,
+  nftMintedApi: 5,
 };
 
 // TODO: Remove and create worker
