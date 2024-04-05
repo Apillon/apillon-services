387ccfc085d683129953af7df1b9a8c70fd5b41c
Tadej <tadej.dev@gmail.com>
2024-04-05 14:39:17 +0200

    fix(nfts): fix column length for contract address

---
+++ services/nfts/src/migration-scripts/migrations/18-change-type-for-collection-contract-address.ts
@@ -0,0 +1,19 @@
+import { DbTables } from '../../config/types';
+
+export async function upgrade(
+  queryFn: (query: string, values?: any[]) => Promise<any[]>,
+): Promise<void> {
+  await queryFn(`
+    ALTER TABLE \`${DbTables.COLLECTION}\`
+      MODIFY COLUMN \`contractAddress\` VARCHAR (50);
+  `);
+}
+
+export async function downgrade(
+  queryFn: (query: string, values?: any[]) => Promise<any[]>,
+): Promise<void> {
+  await queryFn(`
+    ALTER TABLE \`${DbTables.COLLECTION}\`
+      MODIFY COLUMN \`contractAddress\` VARCHAR (42);
+  `);
+}
