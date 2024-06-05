dd8585bded8ecff083ce89871fa233049a643c73
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2024-06-05 10:43:04 +0200

    Add claim contract job seed

---
+++ services/referral/src/migration-scripts/seeds/4-claim-contract-job.ts
@@ -0,0 +1,15 @@
+export async function upgrade(
+  queryFn: (query: string, values?: any[]) => Promise<any[]>,
+): Promise<void> {
+  await queryFn(`
+    INSERT INTO \`job\` (\`status\`, \`name\`, \`interval\`, \`nextRun\`, \`timeout\`)
+    VALUES ('5', 'ClaimContractEventWorker', '0 */1 * * *', NOW(), '3600'); `);
+}
+
+export async function downgrade(
+  queryFn: (query: string, values?: any[]) => Promise<any[]>,
+): Promise<void> {
+  await queryFn(`
+    DELETE FROM job WHERE name = 'ClaimContractEventWorker';
+  `);
+}
