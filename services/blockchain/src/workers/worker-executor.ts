0959e87a8ff08cfbe114312c74ada1cb08078afe
Tadej <tadej.dev@gmail.com>
2024-06-07 10:24:28 +0200

    fix(nfts): fix worker executor for Sepolia and Ethereum

--- services/blockchain/src/workers/worker-executor.ts
+++ services/blockchain/src/workers/worker-executor.ts
@@ -292,6 +292,8 @@ export async function handleSqsMessages(
             executeArg: message?.body,
           });
           break;
+        case WorkerName.TRANSMIT_ETHEREUM_TRANSACTIONS:
+        case WorkerName.TRANSMIT_SEPOLIA_TRANSACTIONS:
         case WorkerName.TRANSMIT_MOONBEAM_TRANSACTIONS:
         case WorkerName.TRANSMIT_MOONBASE_TRANSACTIONS:
         case WorkerName.TRANSMIT_ASTAR_TRANSACTIONS:
@@ -304,6 +306,8 @@ export async function handleSqsMessages(
         // case WorkerName.CRUST_TRANSACTIONS:
         //   await new CrustTransactionWorker(workerDefinition, context).run();
         //   break;
+        case WorkerName.VERIFY_ETHEREUM_TRANSACTIONS:
+        case WorkerName.VERIFY_SEPOLIA_TRANSACTIONS:
         case WorkerName.VERIFY_MOONBEAM_TRANSACTIONS:
         case WorkerName.VERIFY_MOONBASE_TRANSACTIONS:
         case WorkerName.VERIFY_ASTAR_TRANSACTIONS:
