50ab4eb3250e91870bbe5d8f19abd61529c37db0
Tine Mlakar <tine.mlakar@kalmia.si>
2024-06-17 14:26:41 +0200

    ipns republish worker with record lock

--- services/storage/src/migration-scripts/migrations/46-ipns-republishDate-field.ts
+++ services/storage/src/migration-scripts/migrations/46-ipns-republishDate-field.ts
@@ -6,6 +6,7 @@ export async function upgrade(
   await queryFn(`
     ALTER TABLE \`${DbTables.IPNS}\`
     ADD COLUMN \`republishDate\` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER \`status\`,
+    ADD COLUMN \`republishStatus\` INT NOT NULL DEFAULT 5 AFTER \`republishDate\`,
     ADD INDEX \`republish_idx\` (\`republishDate\` ASC) VISIBLE;
     `);
 }
@@ -14,6 +15,9 @@ export async function downgrade(
   queryFn: (query: string, values?: any[]) => Promise<any[]>,
 ): Promise<void> {
   await queryFn(`
-        ALTER TABLE \`${DbTables.IPNS}\` DROP COLUMN \`republishDate\`;
+        ALTER TABLE \`${DbTables.IPNS}\` 
+        DROP COLUMN \`republishDate\`,
+        DROP COLUMN \`republishStatus\`,
+        DROP INDEX \`republish_idx\` ;
     `);
 }
