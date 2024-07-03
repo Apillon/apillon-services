73fe594a0cf1f728f028b2b4931d946d33129de0
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2024-07-03 14:40:25 +0200

    Fix imports

--- services/referral/src/modules/airdrop/airdrop.service.ts
+++ services/referral/src/modules/airdrop/airdrop.service.ts
@@ -12,6 +12,8 @@ import { ServiceContext } from '@apillon/service-lib';
 import { UserAirdropTask } from './models/user-airdrop-task.model';
 import { TokenClaim } from './models/token-claim';
 import { ethers } from 'ethers';
+import { ReferralErrorCode } from '../../config/types';
+import { ReferralCodeException } from '../../lib/exceptions';
 
 export class AirdropService {
   /**
