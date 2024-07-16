06edea25db9b846be78cbaf3d07aa819a27aaf78
Jaka Purg <jaka.purg@gmail.com>
2024-07-16 13:28:28 +0200

    Only fetching files that need review, generate auth token on the go instead of env var, moved destination to constant

--- services/storage/src/scripts/utils/export-files.ts
+++ services/storage/src/scripts/utils/export-files.ts
@@ -1,8 +1,9 @@
 import { MySql, env, runWithWorkers } from '@apillon/lib';
 import * as fs from 'fs';
 import axios from 'axios';
+import { BucketType } from '../../config/types';
 
-const UPLOAD_DESTINATION = env.FILE_EXPORT_DESTINATION;
+const UPLOAD_DESTINATION = '<insert_upload_path>';
 const MIN_DATE = '2024-07-01';
 
 const mysql = new MySql({
@@ -13,12 +14,38 @@ const mysql = new MySql({
   password: env.STORAGE_MYSQL_PASSWORD,
 });
 
+// Project UUIDs with subscription
+const PROJECT_UUIDS_WITH_SUBSCRIPTION = [
+  "'4ebbbca0-cc56-45a5-b25c-e4c822b9d603'",
+  "'435b370d-3f19-4896-b6e5-fe9e1f3cff0d'",
+  "'0ff5dc30-502b-49b0-997e-a70e3888f212'",
+  "'7a0d3782-5a88-4f15-8b88-0513b97348c3'",
+  "'1d0abe7f-c669-4b05-b908-e0918b6a7f6f'",
+  "'0ff5dc30-502b-49b0-997e-a70e3888f212'",
+  "'bd2dd87f-4cf3-488b-aabc-5f3343946a58'",
+  "'3615ca46-7d42-4ed9-9c1a-3d5acdd4c653'",
+  "'e76b258f-0ef2-408c-b87d-65419420551a'",
+];
+
+const getAuthToken = () => {
+  const str = `${env.APILLON_API_INTEGRATION_API_KEY}:${env.APILLON_API_INTEGRATION_API_SECRET}`;
+  return `Basic ${Buffer.from(str).toString('base64')}`;
+};
+
 async function exportFiles() {
   const files = await mysql.paramExecute(
-    `SELECT CID, name FROM file WHERE CID is not null and createTime >= '${MIN_DATE}' order by createTime desc`,
+    `SELECT file.CID, file.name
+    FROM file
+    LEFT JOIN bucket on bucket.id=file.bucket_id
+    WHERE file.CID is not null AND
+    bucket.bucketType <> ${BucketType.NFT_METADATA} AND
+    (bucket.bucketType = ${BucketType.STORAGE} or bucket.project_uuid IN (${PROJECT_UUIDS_WITH_SUBSCRIPTION.join(',')})) AND
+     file.createTime >= '${MIN_DATE}'`,
   );
 
   await runWithWorkers(files, 20, {}, async (file) => {
+    const authToken = getAuthToken();
+
     const link = await axios.get<{
       id: string;
       status: number;
@@ -27,7 +54,7 @@ async function exportFiles() {
       };
     }>(`${env.APILLON_API_URL}/storage/link-on-ipfs/` + file.CID, {
       headers: {
-        Authorization: env.APILLON_API_ACCESS_TOKEN,
+        Authorization: authToken,
       },
     });
     if (link.data.status === 200) {
