dfb1f41616084e91aac7334f0fb940f5944ce0ac
Tine Mlakar <tine.mlakar@kalmia.si>
2024-08-05 11:32:48 +0200

    Add additional request info to acurast job payload

--- modules/acurast-gw-api/src/handler.ts
+++ modules/acurast-gw-api/src/handler.ts
@@ -3,9 +3,26 @@ import type { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const job_uuid = event.requestContext?.domainPrefix;
-  const payload = event.body;
+
+  const blockedHeadersRegex = new RegExp('^(X-|CloudFront-|Via).*$');
+  const headers = Object.keys(event.headers)
+    .filter((key) => !blockedHeadersRegex.test(key))
+    .reduce((obj, key) => {
+      obj[key] = event.headers[key];
+      return obj;
+    }, {});
+
+  const payload = {
+    body: event.body ? JSON.parse(event.body) : {},
+    headers,
+    path: event.path,
+    queryStringParameters: event.queryStringParameters,
+    multiValueQueryStringParameters: event.multiValueQueryStringParameters,
+    pathParameters: event.pathParameters,
+  };
 
   console.log(event);
+  console.log(JSON.stringify(payload));
 
   if (event.httpMethod !== 'POST' || !job_uuid || !payload) {
     return {
@@ -17,7 +34,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   try {
     const serviceResponse = await new ComputingMicroservice(
       null,
-    ).sendJobMessage(payload, job_uuid);
+    ).sendJobMessage(JSON.stringify(payload), job_uuid);
 
     return {
       statusCode: 200,
