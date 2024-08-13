a6eed9c01a4bcfa51cd11d12c7071482944a41f5
Vinko Šmid <vinko.smid@kalmia.si>
2024-08-13 12:15:48 +0200

    fixed link when generating baseUri with IPNS.

--- services/storage/src/workers/prepare-metada-for-collection-worker.ts
+++ services/storage/src/workers/prepare-metada-for-collection-worker.ts
@@ -419,6 +419,7 @@ export class PrepareMetadataForCollectionWorker extends BaseQueueWorker {
             baseUri = await ipfsCluster.generateLink(
               bucket.project_uuid,
               ipnsDbRecord?.ipnsName || data.metadataCid,
+              ipnsDbRecord?.ipnsName ? true : false,
             );
           } else {
             baseUri = ipnsDbRecord?.ipnsName
