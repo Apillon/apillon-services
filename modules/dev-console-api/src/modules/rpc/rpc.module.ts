3d9d46974263f6f160998987ebe58a7fc7a54fd6
Jaka Purg <purg.jaka@gmail.com>
2024-10-18 10:49:08 +0200

    added missing provider for services service

--- modules/dev-console-api/src/modules/rpc/rpc.module.ts
+++ modules/dev-console-api/src/modules/rpc/rpc.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RpcController } from './rpc.controller';
 import { RpcService } from './rpc.service';
+import { ServicesModule } from '../services/services.module';
+import { ServicesService } from '../services/services.service';
 @Module({
   controllers: [RpcController],
-  providers: [RpcService],
+  providers: [RpcService, ServicesService],
 })
 export class RpcModule {}
