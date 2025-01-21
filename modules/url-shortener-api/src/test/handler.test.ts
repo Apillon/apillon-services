41b9d271468e0b985f1a3ec006a224d245b5f31b
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2025-01-21 14:39:10 +0100

    Add handlers for query parameters on short URL

--- modules/url-shortener-api/src/test/handler.test.ts
+++ modules/url-shortener-api/src/test/handler.test.ts
@@ -1,21 +1,34 @@
-import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import { handler } from '../handler'; // replace with your actual file path
+import { APIGatewayEvent } from 'aws-lambda';
+import { handler } from '../handler';
 import { setupTest } from '../../test/helpers/setup';
 import { Stage, releaseStage } from '@apillon/tests-lib';
 import { DbTables } from '@apillon/storage/src/config/types';
 
 describe('Lambda Handler', () => {
   let stage: Stage;
-  const validShortUrlId = 'test1';
+  const validShortUrlId1 = 'test1';
+  const validShortUrlId2 = 'test2';
+  const validShortUrlId3 = 'test3';
+  const validShortUrlId4 = 'test4';
   const validTargetUrl = 'https://ipfs.apillon.io/ipfs/12345';
+  const validTargetUrlWithPath =
+    'https://ipfs.apillon.io/ipfs/12345/path/to/resource';
+  const validTargetUrlWithQuery =
+    'https://ipfs.apillon.io/ipfs/12345?query=param';
+  const validTargetUrlWithPathAndQuery =
+    'https://ipfs.apillon.io/ipfs/12345/path/to/resource?query=param';
 
   beforeAll(async () => {
     stage = await setupTest();
 
     stage.db.storage.paramExecute(`
-    INSERT INTO ${DbTables.SHORT_URL} (id, status, targetUrl)
-    VALUES ('${validShortUrlId}', 5, '${validTargetUrl}')
-   `);
+      INSERT INTO ${DbTables.SHORT_URL} (id, status, targetUrl)
+      VALUES
+      ('${validShortUrlId1}', 5, '${validTargetUrl}'),
+      ('${validShortUrlId2}', 5, '${validTargetUrlWithPath}'),
+      ('${validShortUrlId3}', 5, '${validTargetUrlWithQuery}'),
+      ('${validShortUrlId4}', 5, '${validTargetUrlWithPathAndQuery}')
+    `);
   });
 
   afterAll(async () => {
@@ -25,43 +38,74 @@ describe('Lambda Handler', () => {
   it('should return a 302 redirect with a valid URL', async () => {
     const event: Partial<APIGatewayEvent> = {
       pathParameters: {
-        proxy: validShortUrlId,
+        proxy: validShortUrlId1,
       },
     };
-    const context: Partial<Context> = {};
-
-    const result = (await handler(
-      event as APIGatewayEvent,
-    )) as APIGatewayProxyResult;
+    const result = await handler(event);
 
     expect(result.statusCode).toBe(302);
     expect(result.headers?.Location).toBe(validTargetUrl);
   });
 
+  it('should return a 302 redirect with a valid URL and additional path', async () => {
+    const event: Partial<APIGatewayEvent> = {
+      pathParameters: { proxy: `${validShortUrlId2}/path/to/resource` },
+    };
+
+    const result = await handler(event);
+
+    expect(result.statusCode).toBe(302);
+    expect(result.headers?.Location).toBe(
+      `${validTargetUrlWithPath}/path/to/resource`,
+    );
+  });
+
+  it('should return a 302 redirect with a valid URL and query params', async () => {
+    const event: Partial<APIGatewayEvent> = {
+      pathParameters: { proxy: `${validShortUrlId3}?newQuery=newParam` },
+    };
+
+    const result = await handler(event);
+
+    expect(result.statusCode).toBe(302);
+    expect(result.headers?.Location).toBe(
+      `${validTargetUrlWithQuery}&newQuery=newParam`,
+    );
+  });
+
+  it('should return a 302 redirect with a valid URL, additional path, and query params', async () => {
+    const event: Partial<APIGatewayEvent> = {
+      pathParameters: {
+        proxy: `${validShortUrlId4}/new/path?newQuery=newParam`,
+      },
+    };
+
+    const result = await handler(event);
+
+    expect(result.statusCode).toBe(302);
+    expect(result.headers?.Location).toBe(
+      `${validTargetUrlWithPathAndQuery}/new/path&newQuery=newParam`,
+    );
+  });
+
   it('should return a 400 error when path parameter is missing', async () => {
     const event: Partial<APIGatewayEvent> = {};
 
-    const result = (await handler(
-      event as APIGatewayEvent,
-    )) as APIGatewayProxyResult;
+    const result = await handler(event);
 
     expect(result.statusCode).toBe(400);
     expect(result.body).toBe(JSON.stringify({ message: 'Invalid request' }));
   });
 
-  it('should return a 404 error if short url does not exists', async () => {
+  it('should return a 404 error if short url does not exist', async () => {
     const event: Partial<APIGatewayEvent> = {
-      pathParameters: {
-        proxy: 'test-path',
-      },
+      pathParameters: { proxy: 'non-existent-path' },
     };
-    const result = (await handler(
-      event as APIGatewayEvent,
-    )) as APIGatewayProxyResult;
+    const result = await handler(event);
 
     expect(result.statusCode).toBe(404);
     expect(result.body).toContain(
-      'Short URL key is not valid or does not exists',
+      'Short URL key is not valid or does not exist',
     );
   });
 });
