9a8b2be2b4270ad624107c3c619b26f5d4e193a0
Tadej <tadej.dev@gmail.com>
2025-01-27 12:45:04 +0100

    fix(blockchain): fix blockchain jobs for new EVM chains

--- services/blockchain/src/migration-scripts/seeds/1736424327733-evm-jobs.ts
+++ services/blockchain/src/migration-scripts/seeds/1736424327733-evm-jobs.ts
@@ -13,17 +13,41 @@ const chains = [
   EvmChain.POLYGON_AMOY,
 ];
 
+const TRANSMIT_WORKERS = {
+  [EvmChain.ARBITRUM_ONE]: WorkerName.TRANSMIT_ARBITRUM_ONE_TRANSACTIONS,
+  [EvmChain.ARBITRUM_ONE_SEPOLIA]:
+    WorkerName.TRANSMIT_ARBITRUM_ONE_SEPOLIA_TRANSACTIONS,
+  [EvmChain.AVALANCHE]: WorkerName.TRANSMIT_AVALANCHE_TRANSACTIONS,
+  [EvmChain.AVALANCHE_FUJI]: WorkerName.TRANSMIT_AVALANCHE_FUJI_TRANSACTIONS,
+  [EvmChain.OPTIMISM]: WorkerName.TRANSMIT_OPTIMISM_TRANSACTIONS,
+  [EvmChain.OPTIMISM_SEPOLIA]:
+    WorkerName.TRANSMIT_OPTIMISM_SEPOLIA_TRANSACTIONS,
+  [EvmChain.POLYGON]: WorkerName.TRANSMIT_POLYGON_TRANSACTIONS,
+  [EvmChain.POLYGON_AMOY]: WorkerName.TRANSMIT_POLYGON_AMOY_TRANSACTIONS,
+};
+const VERIFY_WORKERS = {
+  [EvmChain.ARBITRUM_ONE]: WorkerName.VERIFY_ARBITRUM_ONE_TRANSACTIONS,
+  [EvmChain.ARBITRUM_ONE_SEPOLIA]:
+    WorkerName.VERIFY_ARBITRUM_ONE_SEPOLIA_TRANSACTIONS,
+  [EvmChain.AVALANCHE]: WorkerName.VERIFY_AVALANCHE_TRANSACTIONS,
+  [EvmChain.AVALANCHE_FUJI]: WorkerName.VERIFY_AVALANCHE_FUJI_TRANSACTIONS,
+  [EvmChain.OPTIMISM]: WorkerName.VERIFY_OPTIMISM_TRANSACTIONS,
+  [EvmChain.OPTIMISM_SEPOLIA]: WorkerName.VERIFY_OPTIMISM_SEPOLIA_TRANSACTIONS,
+  [EvmChain.POLYGON]: WorkerName.VERIFY_POLYGON_TRANSACTIONS,
+  [EvmChain.POLYGON_AMOY]: WorkerName.VERIFY_POLYGON_AMOY_TRANSACTIONS,
+};
+
 export async function upgrade(
   queryFn: (query: string, values?: any[]) => Promise<any[]>,
 ): Promise<void> {
   const values = chains
     .map(
       (chain) => `
-        ('${WorkerName.TRANSMIT_BASE_TRANSACTIONS}', 0, '*/2 * * * *',
+        ('${TRANSMIT_WORKERS[chain]}', 0, '*/2 * * * *',
          '2024-12-23 10:00:00',
          '{"chain": ${chain}}',
          '{"chain": ${chain}, "channel": 0}', 9, 900),
-        ('${WorkerName.VERIFY_BASE_TRANSACTIONS}', 0, '*/1 * * * *',
+        ('${VERIFY_WORKERS[chain]}', 0, '*/1 * * * *',
          '2024-12-23 10:00:00',
          '{"chain": ${chain}}',
          '{"chain": ${chain}, "channel": 0}', 9, 900)`,
@@ -47,9 +71,9 @@ export async function downgrade(
 ): Promise<void> {
   const names = chains
     .map(
-      () => `
-        '${WorkerName.TRANSMIT_BASE_TRANSACTIONS}',
-        '${WorkerName.VERIFY_BASE_TRANSACTIONS}'`,
+      (chain) => `
+        '${TRANSMIT_WORKERS[chain]}',
+        '${VERIFY_WORKERS[chain]}'`,
     )
     .join(', ');
   await queryFn(`
