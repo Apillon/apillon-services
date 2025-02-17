ba510ab908bc9f9f39b78675cb80f2ef35806ef0
Jaka Purg <purg.jaka@gmail.com>
2025-02-17 08:49:15 +0100

    Type fix

--- services/storage/src/modules/deploy/models/deployment-build.model.ts
+++ services/storage/src/modules/deploy/models/deployment-build.model.ts
@@ -105,7 +105,7 @@ export class DeploymentBuild extends AdvancedSQLModel {
       SerializeFor.UPDATE_DB,
     ],
   })
-  public finishedTime: string;
+  public finishedTime: string | null;
 
   @prop({
     parser: { resolver: stringParser() },
