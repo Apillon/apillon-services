8744142849884b1b35d166fb7719af958ccffda2
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2025-03-18 12:45:21 +0100

    Add stringLengthValidator for password

--- modules/dev-console-api/src/modules/user/dtos/register-user.dto.ts
+++ modules/dev-console-api/src/modules/user/dtos/register-user.dto.ts
@@ -1,5 +1,5 @@
 import { stringParser } from '@rawmodel/parsers';
-import { presenceValidator } from '@rawmodel/validators';
+import { presenceValidator, stringLengthValidator } from '@rawmodel/validators';
 import { ModelBase, PopulateFrom } from '@apillon/lib';
 import { ValidatorErrorCode } from '../../../config/types';
 import { prop } from '@rawmodel/core';
@@ -20,6 +20,16 @@ export class RegisterUserDto extends ModelBase {
   @prop({
     parser: { resolver: stringParser() },
     populatable: [PopulateFrom.PROFILE],
+    validators: [
+      {
+        resolver: presenceValidator(),
+        code: ValidatorErrorCode.USER_PASSWORD_NOT_PRESENT,
+      },
+      {
+        resolver: stringLengthValidator({ minOrEqual: 12, max: 100 }),
+        code: ValidatorErrorCode.USER_PASSWORD_TOO_SHORT,
+      },
+    ],
   })
   public password: string;
 }
